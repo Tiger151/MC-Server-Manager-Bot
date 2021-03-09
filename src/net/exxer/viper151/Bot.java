8b4209a848331108ef34ea26cd073f4e8648a038
Taylor <bobman151@gmail.com>
2021-03-09 17:52:10 -0500

    Initial Commit

---
+++ src/net/exxer/viper151/Bot.java
@@ -0,0 +1,135 @@
+package net.exxer.viper151;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.json.JSONObject;
+
+public class Bot {
+	
+	public static void main(final String args[]) {
+		new Bot();
+	}
+	
+	private final long CHANNEL = 0L;      //Command Channel ID
+	private final String TOKEN = "";	//Bot Token
+	
+	private TextChannel commandChannel;
+	
+	public Bot() {
+		DiscordApi api = new DiscordApiBuilder().setToken(TOKEN).login().join();
+		api.updateActivity("Intializing...");
+		ServerControls control = new ServerControls(api, this);
+		commandChannel = api.getTextChannelById(CHANNEL).get();
+		api.setMessageCacheSize(0, 0);
+		
+		api.addMessageCreateListener(event -> {
+			if(commandChannel.equals(event.getChannel())) {
+				String messageRaw = event.getMessageContent();
+				if(messageRaw.length() >= 2) {
+					if(messageRaw.substring(0, 2).equals("//")) {
+						String message = messageRaw.substring(2);
+						
+						switch(message.toLowerCase()) {
+						case "ip":
+							
+							try {
+								String ip = new BufferedReader(new InputStreamReader(new URL("https://api.ipify.org/").openStream())).readLine();
+								commandChannel.sendMessage(new EmbedBuilder()
+										.setDescription("Current Server IP:\n\n" + ip)
+										.setColor(Color.BLUE)
+										.setTitle("Server IP")
+									);
+							} catch (MalformedURLException e) {
+								e.printStackTrace();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+							
+							break;
+						case "start":
+							
+							control.startServer();
+							
+							break;
+						case "stop":	
+							
+							control.stopServer();
+							
+							break;
+						case "restart":
+							
+							control.restartServer();
+							
+							break;
+						default:
+							commandChannel.sendMessage(new EmbedBuilder()
+								.setDescription("//IP : Get the current IP of the server\n//Start : Start the server\n//Stop : Shutdown the server (No Warning)\n//Restart : Manually restart the server (No Warning)")
+								.setColor(Color.BLUE)
+								.setTitle("Commands")
+							);
+						}
+					}else {
+						event.getMessage().delete();
+					}
+				}
+				if(messageRaw.length() == 1) { //catch 1 char messages
+					event.getMessage().delete();
+				}
+			}
+		});
+		api.getTextChannelById(515020547696885784L).get().sendMessage(new EmbedBuilder()
+				.setDescription("The server will start shortly.")
+				.setColor(Color.YELLOW)
+				.setTitle("Starting Server...")
+			);
+		
+		control.startServerInit();
+		
+		try {
+			TimeUnit.MINUTES.sleep(1);
+			if(isOnline()) {
+				commandChannel.sendMessage(new EmbedBuilder()
+						.setDescription("The server is now online.")
+						.setColor(Color.GREEN)
+						.setTitle("Server Online")
+					);
+			}else {
+				commandChannel.sendMessage(new EmbedBuilder()
+						.setDescription("Unable to determine automatically if the server is online.")
+						.setColor(Color.YELLOW)
+						.setTitle("Server Online")
+					);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}	
+		
+	}
+	
+	public boolean isOnline() {
+		try {
+			String ip = new BufferedReader(new InputStreamReader(new URL("https://api.ipify.org/").openStream())).readLine();
+			JSONObject obj = new JSONObject(new BufferedReader(new InputStreamReader(new URL("https://api.mcsrvstat.us/2/" + ip).openStream())).readLine());
+			return obj.getBoolean("online");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public TextChannel getCommandChannel() {
+		return commandChannel;
+	}
+}
---
+++ src/net/exxer/viper151/ServerControls.java
@@ -0,0 +1,187 @@
+package net.exxer.viper151;
+
+import java.awt.Color;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.concurrent.TimeUnit;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+
+public class ServerControls {
+	private DiscordApi api;
+	private Process server;
+	private boolean isOnline = false;
+	private Bot bot;
+	
+	public ServerControls(DiscordApi api, Bot bot) {
+		this.api = api;
+		this.bot = bot;
+	}
+	
+	public void restartServer() {
+		if(!isOnline) {
+			startServer();
+			return;
+		}
+		
+		bot.getCommandChannel().sendMessage(new EmbedBuilder()
+				.setDescription("The server will restart shortly.")
+				.setColor(Color.YELLOW)
+				.setTitle("Restarting Server...")
+			);
+		
+		api.updateActivity("Restarting Server");
+		
+		runCommand("stop");
+		
+		isOnline = false;
+		
+		try {
+			server = Runtime.getRuntime().exec("./start_server.sh");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		isOnline = true;
+		
+		try {
+			TimeUnit.MINUTES.sleep(2);
+			if(bot.isOnline()) {
+				bot.getCommandChannel().sendMessage(new EmbedBuilder()
+						.setDescription("The server is now online.")
+						.setColor(Color.GREEN)
+						.setTitle("Server Online")
+					);
+			}else {
+				bot.getCommandChannel().sendMessage(new EmbedBuilder()
+						.setDescription("Unable to determine automatically if the server is online.")
+						.setColor(Color.YELLOW)
+						.setTitle("Server Online")
+					);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		api.updateActivity("Server Online");
+	}
+	
+	public void startServer() {
+		if(!isOnline) {
+			bot.getCommandChannel().sendMessage(new EmbedBuilder()
+					.setDescription("The server will start shortly.")
+					.setColor(Color.YELLOW)
+					.setTitle("Starting Server...")
+				);
+			
+			api.updateActivity("Starting Server");
+
+			try {
+				server = Runtime.getRuntime().exec("./start_server.sh");
+				isOnline = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				TimeUnit.MINUTES.sleep(2);
+				if(bot.isOnline()) {
+					bot.getCommandChannel().sendMessage(new EmbedBuilder()
+							.setDescription("The server is now online.")
+							.setColor(Color.GREEN)
+							.setTitle("Server Online")
+						);
+				}else {
+					bot.getCommandChannel().sendMessage(new EmbedBuilder()
+							.setDescription("Unable to determine automatically if the server is online.")
+							.setColor(Color.YELLOW)
+							.setTitle("Server Online")
+						);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}else {
+			
+			bot.getCommandChannel().sendMessage(new EmbedBuilder()
+					.setDescription("The server is allready online. Do you mean //Restart ?")
+					.setColor(Color.GREEN)
+					.setTitle("Server Online")
+				);
+			
+		}
+		
+		api.updateActivity("Server Online");
+	}
+	
+	public void stopServer() {
+		if(isOnline) {
+			bot.getCommandChannel().sendMessage(new EmbedBuilder()
+					.setDescription("The server will shutdown shortly.")
+					.setColor(Color.YELLOW)
+					.setTitle("Stopping Server...")
+				);
+			
+			api.updateActivity("Stopping Server");
+
+			runCommand("stop");
+			
+			isOnline = false;
+			
+			try {
+				TimeUnit.MINUTES.sleep(2);
+				if(!bot.isOnline()) {
+					bot.getCommandChannel().sendMessage(new EmbedBuilder()
+							.setDescription("The server is now offline.")
+							.setColor(Color.RED)
+							.setTitle("Server Closed")
+						);
+				}else {
+					bot.getCommandChannel().sendMessage(new EmbedBuilder()
+							.setDescription("Unable to determine automatically if the server is offline.")
+							.setColor(Color.YELLOW)
+							.setTitle("Server Offline")
+						);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}else {
+			bot.getCommandChannel().sendMessage(new EmbedBuilder()
+					.setDescription("The server is allready offline. Do you mean //Start ?")
+					.setColor(Color.RED)
+					.setTitle("Server Offline")
+				);
+		}
+		
+		api.updateActivity("Server Offline");
+	}
+	
+	public void startServerInit() {
+		
+		api.updateActivity("Starting Server");
+
+		try {
+			server = Runtime.getRuntime().exec("./start_server.sh");
+			isOnline = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		api.updateActivity("Server Online");
+	}
+	
+	private void runCommand(String command) {
+		try(BufferedWriter streamInput = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()))) {
+	        streamInput.write(command);
+	        streamInput.newLine();
+	     }catch (IOException e) {
+	    	 e.printStackTrace();
+	     }
+	}
+	
+}
